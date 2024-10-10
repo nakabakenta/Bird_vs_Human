23221b5d99b93d8fbb430cf08a970176d2b1fe1f
DESKTOP-U0TU0SG\GA2B <h1202315003@hiratagakuen.onmicrosoft.com>
2024-10-10 16:29:51 +0900

    【2024/10/10】

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;//プレイヤーの動く速度
+    //自機ステータス
+    public int hp;   //自機の体力
+    public int power;//自機の攻撃力
+    public int speed;//自機の攻撃速度
+    public int cost; //自機の追加武器入手時の初期残数
+
+    private int moveSpeed;        //自機の初期移動速度
+    private int acceleration = 10;//自機の加減速値
+
+    public GameObject mainBullet;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = 40;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //上移動
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += moveSpeed * transform.up * Time.deltaTime;
+        }
+        //右移動
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position -= moveSpeed * transform.forward * Time.deltaTime;
+        }
+        //下移動
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position -= moveSpeed * transform.up * Time.deltaTime;
+        }
+        //左移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += moveSpeed * transform.forward * Time.deltaTime;
+        }
+        //加速
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            if(moveSpeed < 60)
+            {
+                moveSpeed += acceleration;
+            }
+        }
+        //減速
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (moveSpeed > 20)
+            {
+                moveSpeed -= acceleration;
+            }
+        }
+        //攻撃発射
+        if(Input.GetKey(KeyCode.P))
+        {
+            Instantiate(mainBullet, this.transform.position, Quaternion.identity);
+        }
     }
 }
