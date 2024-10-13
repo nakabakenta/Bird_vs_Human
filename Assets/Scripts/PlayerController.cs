f54f2693d0985c5cca3d5e2080e30251026d5097
HINO_MAIN\hinom <h1202315003@hiratagakuen.onmicrosoft.com>
2024-10-13 13:59:22 +0900

    【2024/10/13】

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 40;
+        moveSpeed = 10;
     }
 
     // Update is called once per frame
